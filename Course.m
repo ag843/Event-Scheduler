cded5f2212681709ced118dbc046ea45df62de83
Ashna Gupta <148406858+ashnagupta843@users.noreply.github.com>
2024-04-13 00:27:35 -0400

    Update Course.m

--- Course.m
+++ Course.m
@@ -1,47 +1,51 @@
 classdef Course < Event
-% A course is a subclass of Event that includes a string courseName
-    
-    properties(Access = private)
-        courseName='';  % The name of the course
+    % Course is a subclass of Event with an additional property for the course name.
+
+    properties (Access = private)
+        courseName = ''; % Name of the course
     end
     
     methods
-        function c = Course(openTime, closeTime, duration, importance, ...
-                            id, name)
-            % Construct course c. The inherited properties are set by
-            % calling Event's contructor.  c.courseName is set to name.
-            if (nargin<5)
-               openTime= 0;
-               closeTime= 0;
-               duration= 0;
-               importance= 0;
-               id= -1;
+        function obj = Course(openTime, closeTime, duration, importance, id, name)
+            % Constructor for the Course class.
+            % Constructs an object by first calling the constructor of the superclass Event.
+            % If all parameters are provided, it sets the course name.
+            
+            % Default values if not all arguments except 'name' are provided
+            if nargin < 5
+                openTime = 0;
+                closeTime = 0;
+                duration = 0;
+                importance = 0;
+                id = -1;
             end
-            c = c@Event(openTime, closeTime, duration, importance, id);
-            if (nargin == 6)
-                c.courseName = name;
+            
+            % Call to superclass (Event) constructor
+            obj = obj@Event(openTime, closeTime, duration, importance, id);
+            
+            % Set course name if provided
+            if nargin == 6
+                obj.courseName = name;
             end
         end
         
-        function courseName = getCourseName(self)
-        % Returns the private access courseName
-            %%%% Write your code below %%%%
-            courseName=self.courseName;
-        
+        function courseName = getCourseName(obj)
+            % Returns the course name.
+            courseName = obj.courseName;
         end
 
-        function draw(self)
-        % Overwrites Event's draw method. Calls Event's draw method, then
-        % also draws the course name as text in the center of the event if 
-        % it's scheduled.
-            %%%% Write your code below %%%%
-         draw@Event(self);
-         if self.scheduledTime~=-1
-             middle=(self.scheduledTime+self.scheduledTime+self.duration)/2;
-             text(middle, self.getId,self.courseName, 'HorizontalAlignment', 'center')
-         end
+        function draw(obj)
+            % Custom draw method for Course objects.
+            % Calls the draw method from Event, then draws the course name if the event is scheduled.
+            
+            % Call superclass draw method
+            draw@Event(obj);
+            
+            % Draw course name if the course is scheduled
+            if obj.scheduledTime ~= -1
+                middle = (obj.scheduledTime + obj.scheduledTime + obj.duration) / 2;
+                text(middle, obj.getId, obj.courseName, 'HorizontalAlignment', 'center');
+            end
         end
-    end %methods
-    
+    end
 end
-
